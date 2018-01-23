31c2e896b62346da7e4303e8a83901af2c2e2815
Jack Song <133342301@163.com>
2018-01-23 17:16:59 +1100

    Fixed WP_Error

--- wc-rest-payment.php
+++ wc-rest-payment.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WC REST Payment
- * Description: WC REST Payment adds in the missing `payment processing REST API` endpoint for `WooCommerce`. 
+ * Description: WC REST Payment adds in the missing REST API endpoint for **process payment** in WooCommerce. 
  * Author: SK8Tech
  * Author URI: https://sk8.tech
  * Version: 1.0.0
@@ -29,21 +29,21 @@ function wc_rest_payment_endpoint_handler() {
 	$error          = new WP_Error();
 
 	if ( empty( $payment_method ) ) {
-		$error->add( 400, __( 'Payment Method is required.', 'woocommerce' ), array( 'status' => 400 ) );
+		$error->add( 400, __( 'Payment Method is required.', 'wc-rest-payment' ), array( 'status' => 400 ) );
 
-		return custom_error_code( $error );
+		return $error;
 
 	}
 	if ( empty( $order_id ) ) {
-		$error->add( 400, __( 'Order ID is required.', 'woocommerce' ), array( 'status' => 400 ) );
+		$error->add( 400, __( 'Order ID is required.', 'wc-rest-payment' ), array( 'status' => 400 ) );
 
-		return custom_error_code( $error );
+		return $error;
 
 	}
 	if ( empty( $payment_token ) ) {
-		$error->add( 400, __( 'Payment Token is required.', 'woocommerce' ), array( 'status' => 400 ) );
+		$error->add( 400, __( 'Payment Token is required.', 'wc-rest-payment' ), array( 'status' => 400 ) );
 
-		return custom_error_code( $error );
+		return $error;
 
 	}
 	
@@ -54,14 +54,14 @@ function wc_rest_payment_endpoint_handler() {
 		$payment_result                   = $wc_gateway_stripe->process_payment( $order_id );
 		if ( $payment_result['result'] === "success" ) {
 			$response['code']    = 200;
-			$response['message'] = __( "Your Payment Successfully", "payment-method" );
+			$response['message'] = __( "Your Payment Successfully", "wc-rest-payment" );
 		} else {
 			$response['code']    = 401;
-			$response['message'] = __( "Please enter valid card details", "payment-method" );
+			$response['message'] = __( "Please enter valid card details", "wc-rest-payment" );
 		}
 	}  else {
 		$response['code'] = 401;
-		$response['message'] = __( "Please select an available payment method. Supported payment method can be found at https://wordpress.org/plugins/wc-rest-payment/#description", "payment-method" );
+		$response['message'] = __( "Please select an available payment method. Supported payment method can be found at https://wordpress.org/plugins/wc-rest-payment/#description", "wc-rest-payment" );
 	}
 	
 
